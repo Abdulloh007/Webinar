0416412946cb03287caaf4b942da72934e83d6cc
Abdulloh007 <sabirovabdulloh2004@gmail.com>
2022-07-22 11:02:37 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+
+	require "system/Routing.php";
+	$url = key($_GET);
+
+
+	$route = new Router();
+	$route->addRoute("/", "main.php");
+	$route->addRoute("/about", "about.php");
+	$route->addRoute("/feedback", "feedback.php");
+
+	$route->route("/".$url);
+
+ ?>
---
+++ system/Routing.php
@@ -0,0 +1,34 @@
+<?php 
+
+/**
+ * Router
+ */
+class Router
+{
+	
+	private $pages = array();
+
+	public function addRoute($url, $path){
+		$this->pages[$url] = $path;
+	}
+	public function route($url){
+		$path = $this->pages[$url];
+		$file_dir = "pages/".$path;
+
+		if($path == ""){
+			require "404.php";
+			die();
+		}
+
+		if(file_exists($file_dir)){
+			require $file_dir;
+		}else{
+			require "404.php";
+			die();			
+		}
+
+	}
+
+}
+
+ ?>
