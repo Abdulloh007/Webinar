1af13657fddffc503b1b6fa30778283e2802ab53
Abdulloh007 <sabirovabdulloh2004@gmail.com>
2022-07-22 17:24:31 +0300

    added new routings

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 	require "App/Routing.php";
 	$url = key($_GET);
 
-
 	$route = new Router();
 	$route->addRoute("/", "main.php");
-	$route->addRoute("/about", "about.php");
-	$route->addRoute("/feedback", "feedback.php");
+	$route->addRoute("/shop", "shop.php");
+	$route->addRoute("/order", "order.php");
+	$route->addRoute("/order", "order.php");
+	$route->addRoute("/single_product", "single_product.php");
+	$route->addRoute("/cart", "cart.php");
 
 	$route->route("/".$url);
 
